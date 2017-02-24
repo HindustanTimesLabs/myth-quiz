24255632d1e8e6cf516e87086d850f14a113f298
Gurman Bhatia <gurmanbh@gmail.com>
2017-02-24 16:23:33 +0530

    json. structure

---
+++ js/app.js
@@ -0,0 +1,50 @@
+require("../css/styles.scss")
+var $ = jquery = require('jquery')
+$(".play").click(function(){
+  $(".intro").fadeOut();
+  setTimeout(function(){
+    $(".quiz").fadeIn();
+  }, 500);
+});
+
+var question_counter = 0
+
+$(".button").click(function(){
+  setTimeout(function(){
+    $(document).scrollTop(0);
+  }, 500);
+
+});
+
+$.getJSON("data/data.json", function(data){
+  $('.total-number').html(data.length)
+  getQuestion(question_counter)
+  $('body').on('click','.answers .button',function(){
+    $('.answers .button').addClass('disabled')
+    updateSentence($(this).attr("data-which"))
+    $('.next.button').removeClass('hide')
+     $('.next.button').on('click',function(){
+      if(question_counter<(data.length-1)){
+        question_counter=question_counter+1
+        getQuestion(question_counter)
+      } else {
+        $('.next.button').html('Review')
+      }
+    })
+  })
+  function getQuestion(index){
+    $('.answers').html('')
+    $('.counter .card-number').html(index+1)
+    $('.next.button').addClass('hide')
+    $('.quiz .question').html(data[question_counter]['question'])
+    $('.results').css('display','none')
+    data[question_counter]['options'].forEach(function(e,i){
+      $('.answers').append('<div class="button c'+(i+1)+'" data-which="'+i+'">'+e.opt+"</div>")
+    })
+  }
+  function updateSentence(index){
+    $('.results').css('display','block')
+    $('.results p').html(data[question_counter]['options'][index]['response'])
+  }
+});
+
